fc060a1f64e52068650ea956b163365e7b6dd89c
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-14 00:30:08 +0000

    Initial commit, straight after the `android create project` command.

---
+++ addressbook/src/net/marcuswhybrow/uni/g54mdp/ae02/addressbook/AddressBookActivity.java
@@ -0,0 +1,15 @@
+package net.marcuswhybrow.uni.g54mdp.ae02.addressbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddressBookActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
